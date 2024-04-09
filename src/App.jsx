ede915a334b86a0725e9c86803290655969c77c8
Sakrid1s <bilinets.vitalik@gmail.com>
2024-04-09 16:37:21 +0300

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import './App.css';
 import { getUnsplashImages } from './api/unsplash-api';
 import SearchBar from './components/searchBar/SearchBar';
 import ImageGallery from './components/imageGallery/ImageGallery';
+import Loader from './components/loader/Loader';
 
 function App() {
   const [image, setImage] = useState([]);
   const [page] = useState(1);
   const [searchValue, setSearchValue] = useState('');
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   // const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     async function fetchImages() {
       try {
         setImage([]);
-        // setLoading(true);
+        setLoading(true);
         const data = await getUnsplashImages(searchValue, page);
         const res = data.results;
         console.log(res);
@@ -23,7 +24,7 @@ function App() {
       } catch (error) {
         console.log(error.message);
       } finally {
-        // setLoading(false);
+        setLoading(false);
       }
     }
     fetchImages();
@@ -36,6 +37,7 @@ function App() {
     <>
       <SearchBar handleSearch={handleSearch} />
       <ImageGallery imageArray={image} />
+      {loading && <Loader />}
     </>
   );
 }
